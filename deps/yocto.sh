324d4d615271fb39f8c2b5aac67b9a6e4e254520
Oscar Andreasson <oscar.andreasson@pelagicore.com>
2016-05-02 15:33:01 +0200

    Initial commit.

---
+++ deps/yocto.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+# Install dependencies
+
+sudo apt-get update
+sudo apt-get install -y git sed wget cvs subversion git-core \
+    coreutils unzip gawk python-pysqlite2 diffstat help2man make gcc \
+    build-essential g++ chrpath libxml2-utils libsdl1.2-dev texinfo
+
---
+++ disks/create_disk.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+disk=$1
+# o  - clear the in memory partition table
+# n  - new partition
+# p  - primary partition
+# 1  - partition number 1
+#" " - default - start at beginning of disk
+#" " - default - full disk
+# w  - write the partition table
+(echo o; echo n; echo p; echo 1; echo " "; echo " "; echo w;) | fdisk $disk
+mkfs.ext4 ${disk}1
+
+# Back up the old .ssh directory, this contains the pubkey for the
+# vagrant tool, w/o this, we can't use "vagrant ssh"
+cp -a /home/vagrant/.ssh /tmp
+
+# Mount the new drive
+mount ${disk}1 /home/vagrant/
+
+# Restore the .ssh backup
+cp -a /tmp/.ssh /home/vagrant/
+
+# Re-initialize the home directory
+cp -r /etc/skel/.* /home/vagrant
+chown vagrant:vagrant /home/vagrant/
