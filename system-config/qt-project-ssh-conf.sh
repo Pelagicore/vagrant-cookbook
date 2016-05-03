d68bc0d81af51dd07494881fe93c3936a2b7a1e5
Oscar Andreasson <oscar.andreasson@pelagicore.com>
2016-05-03 13:54:58 +0200

    Adds repo-init.sh

--- system-config/qt-project-ssh-conf.sh
+++ system-config/qt-project-ssh-conf.sh
@@ -4,12 +4,14 @@ echo "Configuring the SSH client to use the correct user for gits at qt-project.
 
 QT_PROJECT_USER=$1
 if [ "$QT_PROJECT_USER" != "" ] ; then
-mkdir -p ~/.ssh/
-cat >> ~/.ssh/config <<-EOF
+    mkdir -p ~/.ssh/
+
+    ssh-keyscan -p 29418 codereview.qt-project.org | tee -a ~/.ssh/known_hosts
+
+    cat >> ~/.ssh/config <<-EOF
 Host codereview.qt-project.org
     HostName codereview.qt-project.org
     Port 29418
     User $QT_PROJECT_USER
 EOF
-ssh-keyscan -p 29418 codereview.qt-project.org | tee -a ~/.ssh/known_hosts
 fi
---
+++ yocto/repo-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+MANIFEST_REPO=$1
+MENIFEST_REPO_BRANCH=$2
+MANIFEST=$3
+YOCTODIR="$4"
+
+export CONFDIR=~/yocto/build/conf
+
+# Clone recipes
+mkdir -p "$YOCTODIR"
+cd "$YOCTODIR"
+repo init -u "$MANIFEST_REPO" -m "$MANIFEST" -b dev
+repo sync
