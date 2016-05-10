3ee89fb00ea741fa377e1baf495415d0f48ccbdc
Oscar Andreasson <oscar.andreasson@pelagicore.com>
2016-05-10 14:52:36 +0200

    common-build-dependencies.sh: Adds lcov

--- deps/common-build-dependencies.sh
+++ deps/common-build-dependencies.sh
@@ -10,7 +10,7 @@ apt-get install -y libglib2.0-dev
 
 # For pelagicontain
 apt-get install -y libdbus-c++-dev libdbus-c++-1-0v5 libdbus-1-dev libglibmm-2.4-dev libglibmm-2.4 \
-    lxc-dev libpulse-dev unzip bridge-utils
+    lxc-dev libpulse-dev unzip bridge-utils lcov
 
 # For jsonparser
 apt-get install -y libjansson-dev libjansson4
