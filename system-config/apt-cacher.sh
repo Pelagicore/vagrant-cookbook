13a66076b21f5d4ff8622d43380b5c53cf5714f7
Oscar Andreasson <oscar.andreasson@pelagicore.com>
2016-05-13 09:21:59 +0200

    Update apt-cacher.sh

--- system-config/apt-cacher.sh
+++ system-config/apt-cacher.sh
@@ -5,14 +5,14 @@ server=$1
 ping -c1 $server
 retval=$1
 
-if [ "$retval" -ne "0" ] ; then 
+if [ "$retval" != "0" ] ; then 
 
     echo "Adding apt proxy to target" 
 
-    echo -e "Acquire::http::Proxy \\"http://$server:3142\\";\n" > /etc/apt/apt.conf.d/80proxysettings
+    echo -e "Acquire::http::Proxy \"http://$server:3142\";\n" > /etc/apt/apt.conf.d/80proxysettings
 
     apt-get update
 
-elif
+else
     echo "Failed to set apt-proxy to $server"
 fi
