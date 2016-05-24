98d9b9c8a8431d2e8f30647ea02a81ac03d70be6
Oscar Andreasson <oscar.andreasson@pelagicore.com>
2016-05-24 13:40:40 +0200

    cmake-builder.sh Delete build dir just in case

--- build/cmake-builder.sh
+++ build/cmake-builder.sh
@@ -4,10 +4,11 @@ srcdir=$1
 builddir=$srcdir/build
 cmakeargs=$2
 
-echo "Building $srcdir from git repo $gitrepo"
+echo "Building $srcdir from /vagrant dir"
 
 rm -rf $srcdir
 cp -a /vagrant $srcdir
+rm -rf $builddir
 mkdir $builddir
 cd $builddir
 cmake .. $cmakeargs
