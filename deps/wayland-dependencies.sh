a218b4eb4e6b40b56e7730e391c56f31b7c0fe71
Tobias Olausson <tobias.olausson@pelagicore.com>
2016-06-01 13:24:43 +0200

    adds wayland-dependencies.sh

---
+++ deps/wayland-dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Installing wayland related dependencies"
+
+function install {
+    packages="$@"
+
+    retval=100
+    count=0
+
+    if [[ "$retval" -ne "0" && $count -le 5 ]]; then
+        count=$count+1
+        DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy install $packages
+        retval=$?
+    fi
+}
+
+install weston
+
