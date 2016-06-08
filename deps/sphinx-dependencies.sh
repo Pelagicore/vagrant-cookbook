724aca2539e41c2747af34ba42e583ab6cb0e858
Tobias Olausson <tobias.olausson@pelagicore.com>
2016-06-08 12:58:19 +0200

    adds sphinx-dependencies.sh, can be used for projects that use cmake to build sphinx docs

---
+++ deps/sphinx-dependencies.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function install {
+    packages="$@"
+
+    retval=100
+    count=0
+
+    if [[ "$retval" -ne "0" && $count -le 5 ]]; then
+        count=$count+1
+        DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy install $packages
+        retval=$?
+    fi
+}
+
+# Install dependencies (as root)
+sudo apt-get update
+install git python-pip texlive-latex-base texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra aspell
+
+# Install Sphinx (as root)
+sudo pip install Sphinx
+sudo pip install sphinxcontrib-manpage
+sudo pip install hieroglyph
+
