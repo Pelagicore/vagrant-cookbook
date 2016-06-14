4e90c3d66ebb7901e153b3dbd8b8b7e417c04afd
Tobias Olausson <tobias.olausson@pelagicore.com>
2016-06-14 12:22:10 +0200

    apt-cacher: only add cache if server is up

--- system-config/apt-cacher.sh
+++ system-config/apt-cacher.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 server=$1
+port=3142
 
-ping -c1 $server
+curl $server:$port > /dev/null 2>&1
 retval=$1
 
 if [ "$retval" != "0" ] ; then 
-
     echo "Adding apt proxy to target" 
-
     echo -e "Acquire::http::Proxy \"http://$server:3142\";\n" > /etc/apt/apt.conf.d/80proxysettings
-
     apt-get update
-
 else
     echo "Failed to set apt-proxy to $server"
 fi
