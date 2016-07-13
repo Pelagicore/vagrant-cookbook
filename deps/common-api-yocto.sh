ec0d494e19bdee5a5cad695d63c84573919d6bc8
Tobias Olausson <tobias.olausson@pelagicore.com>
2016-07-13 09:44:33 +0200

    adds installation of maven for commonapi builds in yocto

---
+++ deps/common-api-yocto.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Installing version control system dependencies" 
+
+function install {
+    packages="$@"
+    retval=100
+    count=0
+    if [[ "$retval" -ne "0" && $count -le 5 ]]; then
+        count=$count+1
+        DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy install $packages
+        retval=$?
+    fi
+
+    if [[ "$retval" -ne "0" ]]; then
+        echo "Failed to install " $packages
+        exit $retval
+    fi
+}
+
+install maven
