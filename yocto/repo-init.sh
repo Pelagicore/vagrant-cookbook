78baa0bd0ed2afd495d10faabab45bea54690370
Tobias Olausson <tobias.olausson@pelagicore.com>
2016-07-13 09:18:53 +0200

    repo-init.sh: Actually use the manifest branch value

--- yocto/repo-init.sh
+++ yocto/repo-init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-MANIFEST_REPO=$1
-MENIFEST_REPO_BRANCH=$2
-MANIFEST=$3
+MANIFEST_REPO="$1"
+MANIFEST_REPO_BRANCH="$2"
+MANIFEST="$3"
 YOCTODIR="$4"
 
 # Clone recipes
 mkdir -p "$YOCTODIR"
 cd "$YOCTODIR"
-repo init -u "$MANIFEST_REPO" -m "$MANIFEST" -b dev
+repo init -u "$MANIFEST_REPO" -m "$MANIFEST" -b "$MANIFEST_REPO_BRANCH"
 repo sync
