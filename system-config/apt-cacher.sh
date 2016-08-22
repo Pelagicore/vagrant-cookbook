8320d7cdbf8aba79c2862dfdeeb66e2dbbf7e990
Oscar Andreasson <oscar.andreasson@pelagicore.com>
2016-08-22 10:51:38 +0200

    apt-cacher.sh: Updated to ping before curling the url to reduce timeout values

--- system-config/apt-cacher.sh
+++ system-config/apt-cacher.sh
@@ -3,10 +3,16 @@
 server=$1
 port=3142
 
-curl $server:$port > /dev/null 2>&1
-retval=$1
+retval=1
+ping -c 1 $server
+pingretval=$?
+if [ $pingretval == 0 ] ; then 
+    echo "hihihi"
+    curl $server:$port > /dev/null 2>&1
+    retval=$?
+fi 
 
-if [ "$retval" != "0" ] ; then 
+if [ "$retval" == "0" ] ; then 
     echo "Adding apt proxy to target" 
     echo -e "Acquire::http::Proxy \"http://$server:3142\";\n" > /etc/apt/apt.conf.d/80proxysettings
     apt-get update
