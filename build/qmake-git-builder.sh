366589ae020107db8876aa548802e361e1d3cdaf
Erik Botö <erik.boto@pelagicore.com>
2016-10-14 07:16:26 +0200

    Add snippet used to build qmake based projects
    Signed-off-by: Erik Botö <erik.boto@pelagicore.com>

---
+++ build/qmake-git-builder.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# vagrant-cookbook
+# Copyright (C) 2016 Pelagicore AB
+#      
+# Permission to use, copy, modify, and/or distribute this software for 
+# any purpose with or without fee is hereby granted, provided that the 
+# above copyright notice and this permission notice appear in all copies. 
+#  
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL 
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED  
+# WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR 
+# BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES 
+# OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, 
+# WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, 
+# ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS 
+# SOFTWARE.
+#   
+# For further information see LICENSE
+# 
+# 
+# Usage: qmake-git-builder.sh <srcdir> <gitrepo> <qmakepath> <qmakeargs> \
+#              <.pro file>
+# 
+# Download git repo containing a qmake build system into srcdir and build 
+# in srcdir/build using qmakeargs to qmake. 
+#
+
+srcdir=$1
+builddir=$srcdir/build
+gitrepo=$2
+qmakepath=$3
+qmakeargs=$4
+projectfile=$5
+
+echo "Building $srcdir from git repo $gitrepo"
+
+rm -rf $srcdir
+git clone $gitrepo $srcdir
+mkdir $builddir
+cd $builddir
+$qmakepath ../$projectfile $qmakeargs
+make && sudo make install
+
