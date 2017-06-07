cc3589c77478cc2f348826befa19b66a45904b42
Joakim Gross <joakim.gross@pelagicore.com>
2017-06-07 08:58:55 +0200

    yocto/fetch-sources-for-recipes: Fixed loop logic
    The fetchall step will now be tried COUNTER times.
    
    Signed-off-by: Joakim Gross <joakim.gross@pelagicore.com>

--- yocto/fetch-sources-for-recipes.sh
+++ yocto/fetch-sources-for-recipes.sh
@@ -28,10 +28,10 @@ echo "Fetching all the sources. Try $COUNTER times in case of a bad connection."
 cd "$YOCTO_DIR/sources/poky/"
 source oe-init-build-env ../../build
 
-COUNTER=0
-while [  $COUNTER -lt $COUNTER ]; do
-    echo COUNTER $COUNTER
-    let COUNTER+=1
+I=0
+while [  $I -lt $COUNTER ]; do
+    echo "Try number: $I"
+    let I+=1
     sleep 1
     time bitbake -c fetchall $IMAGES
     if [ $? -eq 0 ]; then
