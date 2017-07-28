6c904cb81fafece5aee8c9a814e72f84c50efabf
Tobias Olausson <tobias.olausson@pelagicore.com>
2017-07-28 09:23:16 +0200

    deps: add python3 along python2 stuff
    Signed-off-by: Tobias Olausson <tobias.olausson@pelagicore.com>

--- deps/softwarecontainer-dependencies.sh
+++ deps/softwarecontainer-dependencies.sh
@@ -44,7 +44,7 @@ function install {
 # For softwarecontainer
 install libdbus-1-dev libglibmm-2.4-dev libglibmm-2.4 \
         unzip bridge-utils lcov libjansson-dev libjansson4 \
-        dbus-x11 libcap-dev libtool
+        dbus-x11 libcap-dev libtool python3-dev
 
 apt-get remove --allow-downgrades --allow-remove-essential --allow-change-held-packages -fuy lxcfs lxc2 lxc-dev lxc-common
 
@@ -54,6 +54,6 @@ git clone git://github.com/lxc/lxc -b stable-2.0
 cd lxc
 
 ./autogen.sh
-./configure --prefix=/usr --enable-capabilities
+./configure --prefix=/usr --enable-capabilities --enable-python
 
 make && make install
